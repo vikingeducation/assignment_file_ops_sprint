7a230fb04cc2fa17e4e6b1573f0fd31f27dc7b53
Dan Berger <dsberger@gmail.com>
2015-02-02 14:41:36 -0800

    working dictionary loader

---
+++ dictionary.rb
@@ -0,0 +1,13 @@
+require_relative 'dictionary_loader'
+require_relative 'dictionary_analyzer'
+
+class Dictionary
+  attr_reader :dictionary
+  def initialize
+    print "What is the name of the dictionary you want to load?\n > "
+    @dictionary_name = gets.chomp
+    @dictionary = DictionaryLoader.new(@dictionary_name).dictionary
+  end
+end
+
+# Create a class (e.g. Dictionary) to handle the user interaction loop. The first prompt is to read in a dictionary file.
