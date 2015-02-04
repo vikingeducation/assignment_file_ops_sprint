5eb44a2b45bd9f2991a53f5b25f179c7f23abb82
Dan Berger <dsberger@gmail.com>
2015-02-04 13:33:51 -0800

    analyzer works manually but interface with dictionary needs help

--- dictionary.rb
+++ dictionary.rb
@@ -2,11 +2,18 @@ require_relative 'dictionary_loader'
 require_relative 'dictionary_analyzer'
 
 class Dictionary
-  attr_reader :dictionary
+  attr_reader :dictionary, :analyzer
   def initialize
     print "What is the name of the dictionary you want to load?\n > "
     dictionary_name = gets.chomp
     @dictionary = DictionaryLoader.new(dictionary_name).words
+    @analyzer = DictionaryAnalyzer.new(dictionary)
+    print "How would you like to search?\n1 for an exact match\n2 for partial matches\n3 to match beginning of word\n4 to match end of word\n > "
+    search_style = gets.chomp
+    print "Enter your search string\n > "
+    search_string = gets.chomp
+    search_results = analyzer.search(search_style,search_string)
+    print "I found #{search_results.length} results."
   end
 end
 
--- dictionary_analyzer.rb
+++ dictionary_analyzer.rb
@@ -1,12 +1,68 @@
-# Create a class (e.g. DictionaryAnalyzer) which takes a dictionary and contains methods that provide simple statistics for word count and how many words occur starting with each letter of the alphabet. Display these to the user when the dictionary is successfully read in.
-
 class DictionaryAnalyzer
 
-  attr_reader
+  attr_reader :dictionary
 
   def initialize(dictionary)
     @dictionary = dictionary
+    puts "total word count: #{@dictionary.length}"
+    print_word_count_summary
+  end
 
+  def print_word_count_summary
+    first_letter_counter.each do |count|
+      puts "#{count[0]}: #{count[1]}"
+    end
+  end
 
+  def first_letter_counter
+    @dictionary.each_with_object(initial_letter_hash) do |word, word_count_by_letter|
+      first_letter = word[0].upcase
+      word_count_by_letter[first_letter] += 1
+    end
+  end
+
+  def initial_letter_hash
+    ("A".."Z").each_with_object({}) do |letter, letter_count|
+      letter_count[letter] = 0
+    end
+  end
+
+  def template_search
+    @dictionary.select do |entry|
+      entry =~ yield
+    end
+  end
+
+  def exact_match(search_string)
+    template_search { /^#{search_string}$/ }
+  end
+
+  def partial_match(search_string)
+    template_search { /#{search_string}/ }
+  end
+
+  def begins_with_match(search_string)
+    template_search { /^#{search_string}/ }
+  end
+
+  def ends_with_match(search_string)
+    template_search { /#{search_string}$/ }
+  end
+
+  def search(search_style,search_string)
+    case search_style
+    when 1
+      exact_match search_string
+    when 2
+      partial_match search_string
+    when 3
+      begins_with_match search_string
+    when 4
+      ends_with_match search_string
+    end
   end
 end
+
+# Next allow the user to perform one of four types of searches: exact matches, partial matches, "begins with" and "ends with". The results should return the full word, regardless of match type.
+
+# Once the search is complete, display the number of matches and prompt the user whether to save the results to a file or display to the screen (puts).
