7451b0e16851a20dcaffb9029016bb4b10f165f8
Alice Fung <alice.o.fung@gmail.com>
2015-07-07 13:51:22 -0400

    add partial save method

--- dictionary.rb
+++ dictionary.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+
 class Dictionary
 
   def initialize
@@ -23,7 +25,73 @@ class Dictionary
     filename = gets.chomp
     get_dictionary(filename) if File.exist? (filename)
   end
+
+  def run
+    load_file
+    result = perform_search(search_type)
+    display_matches(result)
+    display_or_save(result)
+  end
+
+  def display_matches(result)
+    puts "There were #{result.length} matches."
+  end
+
+  def display_or_save(result)
+    puts "Do you want to (1) save the results or (2) display to screen?"
+    input = gets.chomp
+    case input
+    when 1
+      save_to_file(result)
+    when 2
+      p(result)
+    end
+  end
+
+  def save_to_file(result)
+    puts "Name your file:"
+    file_name = gets.chomp
+    if File.exist? file_name
+    save(result)
+
+  end
+
+  def save(result)
+    my_dump = YAML::dump(result)
+    my_file
+  end
+
+
+  def perform_search(search_type)
+    puts "What are we looking for?"
+    input = gets.chomp
+    case search_type
+    when 1
+      @dict.exact_match(input)
+    when 2
+      @dict.partial_match(input)
+    when 3
+      @dict.begins_with(input)
+    when 4
+      @dict.ends_with(input)
+    end
+  end
+
+  def search_type
+    puts "(1) exact match"
+    puts "(2) partial match"
+    puts "(3) begins with"
+    puts "(4) ends with"
+    input = 0
+    loop do
+      input = gets.chomp
+      break if (1..4).include? input
+      puts "Please enter option 1-4:"
+    end
+    input
+  end
 end
+
 
 class DictionaryLoader
 
@@ -71,7 +139,7 @@ class DictionaryAnalyzer
 
   def ends_with(input)
     input = input.downcase
-    return @dict.reject{|word| (/.*#{input}$/ =~ word.downcase).nil?}
+    return @dict.reject{|word| (/\w+#{input}\b/ =~ word.downcase).nil?}
   end
 
 
