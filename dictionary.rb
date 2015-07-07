f7625d82e51817caead211fdc978f71367db24c4
Garrett <garrettweinert@gmail.com>
2015-07-07 10:50:58 -0700

    working on partial matches

--- dictionary.rb
+++ dictionary.rb
@@ -1,8 +1,14 @@
 class Dictionary
+  
   attr_reader :dic
 
   def initialize
     @dic=DictionaryLoader.new.dic
+    dic_analyzer = DictionaryAnalyzer.new(@dic)
+    puts dic_analyzer.word_count
+    puts dic_analyzer.count_words_beg_with("a")
+    puts dic_analyzer.exact_match("apple")
+
   end
 
 end
@@ -11,7 +17,8 @@ class DictionaryLoader
   attr_reader :dic
   def initialize(filename="5desk.txt")
     @dic=File.readlines(filename)
-    
+    @dic.map! {|word| word.chomp}
+
   end
 
 
@@ -24,11 +31,23 @@ class DictionaryAnalyzer
   end
 
   def word_count
+    # @dictionary
     @dictionary.length
+  end
 
+  def count_words_beg_with(letter)
+    @dictionary.select {|word| word[0].downcase == letter.downcase}.length
+  end
+
+  def exact_match(word)
+    @dictionary.include?(word.downcase) ? word.downcase : false
+  end
+
+  def partial_match(word)
+    
   end
 
 end
-
-puts DictionaryAnalyzer.new(Dictionary.new.dic).word_count
-
+d = Dictionary.new
+# dic_arr = DictionaryAnalyzer.new(Dictionary.new.dic)
+# puts dic_arr.count_words_beg_with("a")
