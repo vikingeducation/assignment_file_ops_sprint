1a1cca60c08b9179a8a6cbefcc7d6e076aaf4759
Joseph Lozano <lozanojoseph@gmail.com>
2015-07-07 10:25:36 -0700

    dictionary io

--- dictionary.rb
+++ dictionary.rb
@@ -1,7 +1,86 @@
 class Dictionary
 
+  def initialize
+
+    dict = Dictionary_Loader.new("5desk.txt")
+
+    dictionary = dict.load if get_input?
+
+    analyzer = DictionaryAnalyzer.new(dictionary)
+
+    analyzer.word_count
+
+  end
+
+  def get_input?
+    puts "Would you like to load the dicitonary? (Y) for yes."
+    input = gets.chomp.upcase
+    true if input == 'Y'
+  end
+
+
 end
 
 class Dictionary_Loader
 
+  def initialize(file)
+
+    @file = file
+
+  end
+
+  def load
+
+   dict = File.open(@file, "r")
+   return dict.read
+   dict.close
+
+
+
+  end
+
+
 end
+
+class DictionaryAnalyzer
+
+  def initialize(dictionary)
+
+    @dictionary = dictionary
+
+  end
+
+
+  def word_count
+    regex = /\n/
+    string = @dictionary
+    our_match = string.captures(regex)
+
+    p our_match
+
+  end
+
+
+  def beginning_letter_count
+
+  end
+
+end
+
+
+
+dictionary = Dictionary.new
+
+
+
+
+
+
+
+
+
+
+
+
+
+
