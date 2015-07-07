df3327021a0563996ce22be06f1625a33d4e8abc
michelletoussaint <toussaint.michelle@gmail.com>
2015-07-07 14:56:09 -0400

    Fixing initial analysis

--- dictionary.rb
+++ dictionary.rb
@@ -10,11 +10,20 @@ class Dictionary
 
   end
 
+  def user_interaction
+
+    choose_file
+    analyze_dictionary
+    # search_dictionary
+
+  end
+
   def choose_file
 
     puts "Enter the file name you want to read:"
     @file_name = gets.chomp
     @dict = @loader.load_file(@file_name)
+    
 
   end
 
@@ -24,6 +33,34 @@ class Dictionary
     analyzer.initial_analysis
 
   end
+
+  # def search_dictionary
+
+  #   type = type_search
+
+  #   puts = "What do you want to search"
+  #   search_term = gets.chomp
+
+  #   if type == 'e'
+  #     exact_search(search_term)
+  #   elsif type == 'p'
+  #     partial_search(search_term)
+  #   elsif type == 'b'
+  #     begins_search(search_term)
+  #   else
+  #     end_search(search_term)
+  #   end
+        
+
+  # end
+
+  # def type_search
+
+  #   puts "What kind of search do you want to do? exact(e), partial(p), begins with(b), ends with(e)"
+  #   type = gets.chomp
+  #   return type if ['e', 'p', 'b', 'e'].include(type)
+
+  # end
 
 end
 
@@ -51,20 +88,22 @@ class DictionaryAnalyzer
 
   def initial_analysis
 
-    dict_string = @dict.join(" ") + ' '
-    # regex = /^(a|A)/
-
-    # a_array = dict_string.scan(regex)
-    # a_array.lenght
-
-    
     puts "This dictionary contains #{@dict.length} words"
 
+    # dict_string = @dict.join(" ") + ' '
+    
+    # ('a'..'z').each do |l|
+    #   regex = Regexp.new("(#{l}|#{l.upcase})[a-zA-Z]*\\s")
+    #   array = dict_string.scan(regex)
+    #   puts "This dictionary contains #{array.length} words starting with letter #{l}."
+    # end
+
     ('a'..'z').each do |l|
-      regex = Regexp.new("(#{l}|#{l.upcase})[a-zA-Z]*\\s")
-      array = dict_string.scan(regex)
-      puts "This dictionary contains #{array.length} words starting with letter #{l}."
+
+      regex = Regexp.new("^#{l}",true)
+
     end
+
 
     end
 
