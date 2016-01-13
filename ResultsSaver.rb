10c3c792ec16076c2de5bf7e125089f7478ebba4
koziscool <kozjob@yahoo.com>
2016-01-12 19:23:21 -0600

    ResultsSaver

--- ResultsSaver.rb
+++ ResultsSaver.rb
@@ -10,15 +10,20 @@ class ResultsSaver
     def write_results_to_file
         if overwrite
             out = File.open(out_file_name, "w")
-            results.each do | word |
+            @results.each do | word |
                 out.write(word + "\n")
             end
         else
-            results.each do | word |
+            @results.each do | word |
                 puts word
             end
         end
     end
 end
 
+
+r = ResultsSaver.new( ["check", "this", "out"] )
+r.overwrite = false
+r.out_file_name = "someoutput.txt"
+r.write_results_to_file
 
