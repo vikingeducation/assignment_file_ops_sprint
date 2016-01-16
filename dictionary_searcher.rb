9951ef4c0283f6327937445ae8bedce83b4c9e6d
Kelsey James <kelseybjames@gmail.com>
2016-01-16 11:18:43 -0800

    start

---
+++ dictionary_searcher.rb
@@ -0,0 +1,22 @@
+class DictionarySearcher
+
+  def initialize(dictionary)
+    @dictionary = dictionary
+  end
+
+  def exact_match
+
+  end
+
+  def partial_match
+
+  end
+
+  def begins_with
+
+  end
+
+  def ends_with
+
+  end
+end
