de8a6f774ce0523dd34e180f903ab43dfc5aef2e
Deepa Kackar <dkackar99@yahoo.com>
2016-01-16 11:41:57 -0800

    created dictui

--- dictionaryui.rb
+++ dictionaryui.rb
@@ -1,29 +1,56 @@
 
-
 class DictionaryUI
+  attr_accessor :letter_count, :word_count, :dict
 
+  def initialize
+    @dict = nil
+    @letter_count = {}
+    @word_count = 0
+  end  
 
   def ask_user_input
 
-    file = ""
-    unti file != ""
+    dict_file = ""
+
+    until dict_file != ""
       print "Where is your dictionary? ('q' to quit) "
-      file = gets.chomp
+      dict_file = gets.chomp
+      puts dict_file
 
-      if File.file?
-         dict = File.readlines(file)
-         @count = dict.size 
+      if File.file(dict_file)?
+         @dict = File.readlines(dict_file)
+        
+         #@word_count = @dict.size
 
-         puts "No of words is : @count"
+         #puts "No of words is : #{@word_count}"
 
       end
+
     end
 
   end
 
 
   def count_by_letter
-
+    letter_count = {}
+    dict = @dict.join("\n")
+    
+    (a..z).to_a.each do |letter|
+      count = dict.scan(/^#{letter}/i)
+      letter_count[letter] = count
+    end
+  
+    puts letter_count
+    letter_count
+  
   end
 
-end  
+  def process_dict
+    ask_user_input
+    count_by_letter
+  end
+
+end 
+
+dict = DictionaryUI.new
+dict.process_dict
