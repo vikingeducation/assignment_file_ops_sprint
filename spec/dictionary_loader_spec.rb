3c38076d9f91411163e1f5beae44bea0e81c2069
Vishal Lama <vishal.lama@gmail.com>
2016-07-12 12:42:18 -0600

    Add ClassLoader and ClassLoader::load

--- spec/dictionary_loader_spec.rb
+++ spec/dictionary_loader_spec.rb
@@ -2,10 +2,18 @@ require 'dictionary_loader'
 
 describe 'DictionaryLoader' do
 
+  let(:dictionary_file){ "5desk.txt" }
+  let(:dictionary){ DictionaryLoader.load(dictionary_file) }
+
   describe '.load' do
-    it "Loads a file" do
-      allow(DictionaryLoader).to receive(:load)
+    it "correctly loads the first element from dictionary text file" do
+      expect(dictionary[0]).to eq("A")
+    end
+
+    it "correctly loads the last element from dictionary text file" do
+      expect(dictionary[-1]).to eq("Zzz")
     end
   end
 
 end
+
