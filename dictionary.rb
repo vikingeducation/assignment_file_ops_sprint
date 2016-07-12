3ad9bfd21c61a4c1e961c79b6ce1814127d1c5b5
Alex Lach <alexglach@gmail.com>
2016-07-12 13:58:36 -0400

    done with searches

--- dictionary.rb
+++ dictionary.rb
@@ -1,15 +1,25 @@
+require_relative 'dictionary_loader'
+require_relative 'dictionary_searcher'
+
 class Dictionary
   attr_reader :dictionary
   
   def initialize
-    @dictionary = DictionaryLoader.new
+    @dictionary = DictionaryLoader.new.dictionary
     run
   end
 
   def run
     puts "Dictionary succesfully loaded"
-    
+    puts "Your dictionary containts #{@dictionary.size} words"
+    puts "Word frequency by starting letter:"
+    display_word_count
+  end
 
+  def display_word_count
+    ("a".."z").each do |letter|
+      puts "#{letter}: #{@dictionary.count{|word| word[0].downcase == letter}}"
+    end
   end
 
 end
---
+++ dictionary_searcher.rb
@@ -0,0 +1,28 @@
+require_relative 'dictionary'
+
+class DictionarySearcher
+
+  def initialize(dictionary)
+    @dictionary = dictionary.dictionary
+  end
+
+  def exact_matches(search_term)
+    puts @dictionary.select {|word| word.downcase == search_term}
+  end
+
+  def partial_matches(search_term)
+    puts @dictionary.select {|word| word.chars.join.include?(search_term.chars.join)}
+  end
+
+  def begins_with_matches(search_term)
+    puts @dictionary.select {|word| word.start_with?(search_term)}
+  end
+
+  def ends_with_matches(search_term)
+    puts @dictionary.select {|word| word.end_with?(search_term)}
+  end
+
+
+
+
+end
