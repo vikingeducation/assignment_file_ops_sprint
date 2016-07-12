4e2fc69db22442ffff6a7c07b5835bd33699ea42
Hannah Squier <hannahsquier@gmail.com>
2016-07-12 10:08:03 -0700

    add dictionaryui and dictionary-loader classes

---
+++ dictionary.rb
@@ -0,0 +1,5 @@
+class Dictionary
+  def initialize(dict_arr)
+    @dict_arr = dict_arr
+  end
+end
---
+++ dictionaryui.rb
@@ -0,0 +1,10 @@
+class DictionaryUI
+
+
+  def run
+    puts "Where is your dictionary? ('q' to quit)"
+    path = gets.chomp
+  end
+
+   
+end
