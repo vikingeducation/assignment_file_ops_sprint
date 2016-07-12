76bbd4d9bf5dc9e10f02f206c0d757e3c39f007c
Mike Lee <asackofwheat@gmail.com>
2016-07-12 13:39:47 -0400

    finishing up searches

---
+++ dictionarysearcher.rb
@@ -0,0 +1,44 @@
+class DictionarySearcher
+
+  def self.quick_stats(dictionary_array)
+    num_of_words = dictionary_array.length
+    frequencies_hash = {}
+    dictionary_array.each do |word|
+      frequences_hash[word[0].upcase] += 1
+    end
+    [num_of_words, frequencies_hash]
+  end
+
+  def self.exact?(word)
+    dictionary_array.include?(word)
+  end
+
+  def partial(word)
+    match_array = []
+    dictionary_array.each do |dictionary_word|
+      if dictionary_word.include?(word)
+        match_array << dictionary_word
+      end
+    end
+    match_array
+  end
+
+  def begins_with(word)
+    match_array = []
+    regex = /^#{word}/i
+    dictionary_array.each do |dictionary_word|
+      if regex =~ dictionary_word != nil
+        match_array << dictionary_word
+      end
+    end
+  end
+
+  def ends_with(word)
+    match_array = []
+    regex = /#{word}$/i
+    dictionary_array.each do |dictionary_word|
+
+    end
+  end
+
+end
