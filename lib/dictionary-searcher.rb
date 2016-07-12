a4284772d46c7128a1274945ed998865d27a7deb
Hannah Squier <hannahsquier@gmail.com>
2016-07-12 11:46:02 -0700

    added regex for matches

--- lib/dictionary-searcher.rb
+++ lib/dictionary-searcher.rb
@@ -9,39 +9,50 @@ class DictionarySearcher
     puts "How would you like to search? (exact, partial, begins, ends)"
     input_type = gets.chomp
     puts "what would you like to search?"
-    input_str = gets.chomp
-    # use input to select search method & desired string
-    case input_type
-    when "exact"
-      exact_matches(input_str)
-    when "partial"
-      partial_matches(input_str)
-    when "begins"
-      begins_with_matches(input_str)
-    when "ends"
-      ends_with_matches(input_str)
-    else
-      puts "please enter a valid search method!"
-      ask_user_message
-    end
+    input_str = gets.chomp    
+
+    puts input_type_to_method(input_type, input_str)
+ 
   end
 
-  def exact_matches(input)
+  def input_type_to_method(input_type, input_str)
+    string_to_method = {
+      "exact" => method(:exact)
+      "partial" => method(:partial)
+      "begins" => method(:begins)
+      "ends" => method(:ends)
+    }
+    string_to_method[input_type].call(input_str)
+  end
+
+  def exact_matches(input_str)
     matches = dict.dict_arr.select do |entry|
-      entry === /^#{input}$/
+      /^#{input_str}$/ === entry
     end
     matches
   end
 
-  def partial_matches(input)
-
+  def partial_matches(input_str)
+    matches = dict.dict_arr.select do |entry|
+      /#{input_str}/ === entry
+    end
+    matches
   end
 
-  def begins_with_matches(input)
+  def begins_with_matches(input_str)
+    matches = dict.dict_arr.select do |entry|
+      /^#{input_str}/ === entry 
+    end
+    matches
   end
 
-  def ends_with_matches(input)
+  def ends_with_matches(input_str)
+    matches = dict.dict_arr.select do |entry|
+      /#{input_str}$/ === entry
+    end
+    matches
   end
+
 
   def validate_str?(str)
     raise ArgumentError("Please enter a string") unless str === /[a-zA-Z]/
--- lib/dictionaryui.rb
+++ lib/dictionaryui.rb
@@ -1,10 +1,22 @@
+require 'dictionary-loader'
+
+
 class DictionaryUI
+  attr_reader :loaded_dictionary
+  
+  def initialize
+    @loaded_dictionary = nil
+  end
 
   def run
     puts "Where is your dictionary? ('q' to quit)"
     path = gets.chomp
-    loaded_dictionary = DictionaryLoader.new(path)
+    @loaded_dictionary = DictionaryLoader.new(path)
+    @loaded_dictionary.get_dictionary
   end
 
    
 end
+
+d = DictionaryUI.new
+d.run
