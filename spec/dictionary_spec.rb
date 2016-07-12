8f7c2308a3601ba069aa4ac8c44882b41748162e
cjvirtucio87 <cjv287@gmail.com>
2016-07-12 11:56:21 -0700

    Working on loop in run method.

--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -10,6 +10,15 @@ end
 
 describe DictionaryUI do
   let(:dict_ui) { DictionaryUI.new }
+  
+  describe "#run" do
+    it "raises a ENOENT error if the file doesn't exist" do
+      allow(STDOUT).to receive(:puts)
+      allow(STDIN).to receive(:gets).and_return("fake_file.txt","fake_dict.txt")
+      expect { dict_ui.run }.to raise_error(SystemCallError)
+    end
+  end
+
   describe "#prompt_for_input" do
     # it "prints the prompt" do
     #   expect(STDOUT).to receive(:puts)
@@ -22,9 +31,9 @@ end
 
 describe DictionaryLoader do
   it "transfers words from a file to an array" do
-    expect(DictionaryLoader.load("fake_dict.txt"))
+    expect(DictionaryLoader.load("5desk.txt"))
       .to be_an_instance_of(Array)
-    p DictionaryLoader.load("fake_dict.txt")
+    p DictionaryLoader.load("5desk.txt")
   end
 end
 
@@ -33,6 +42,9 @@ end
 
 describe ResultsSaver do
 end
+
+
+
 
 
 #controller dictionary
