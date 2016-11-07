8d53c2bb2caa2b24898d479379ef6701d24365be
John Sawyer <sawyer@sawyermerchant.com>
2016-11-07 18:40:00 -0500

    Dictionary Searcher functions

--- lib/dictionary_searcher.rb
+++ lib/dictionary_searcher.rb
@@ -1,11 +1,16 @@
 class DictionarySearcher
+  attr_reader :dictionary
 
   def initialize(dictionary)
     @dictionary = dictionary
   end
 
   def word_count
-    
+    dictionary.words.count
+  end
+
+  def word_frequency_by_letter
+    dictionary.words.group_by{ |w| w[0] }
   end
 
 end
--- spec/dictionary_searcher_spec.rb
+++ spec/dictionary_searcher_spec.rb
@@ -1,14 +1,14 @@
-describe DictionarySearch do
+require 'dictionary_searcher'
+
+describe DictionarySearcher do
 
   describe "#word_count" do
 
-    let(:dictionary) { instance_double(words: ["meow", "argh", "mama mia!"]) }
+    let(:dictionary) { instance_double("Dictionary",words: ["meow", "argh", "mama mia!"]) }
     let(:searcher) { DictionarySearcher.new(dictionary) }
 
     it "returns number of words in your dictionary as integer" do
       expect(searcher.word_count).to eq(3)
     end
-
   end
-
 end
