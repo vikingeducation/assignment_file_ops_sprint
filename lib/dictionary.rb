d339bc8968fde5bb31d3503ea3772a377a753277
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2016-11-07 18:25:21 -0500

    Implement matching functionality

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,6 +1,12 @@
-class Dictionary
 
+require 'forwardable'
+
+class Dictionary
+  extend Forwardable
+  include Enumerable
   attr_reader :words
+
+  def_delegator :words, :each
 
   def initialize(args = {})
     @words = args[:words]
--- lib/dictionary_searcher.rb
+++ lib/dictionary_searcher.rb
@@ -1,25 +1,38 @@
 class DictionarySearcher
 
-  attr_accessor :dictionary
+  attr_reader :dictionary, :dictionary_length, :midpoint
 
   def initialize(args = {})
     @dictionary = args[:dictionary]
   end
 
-  def exact_match(term)
+  def dictionary=(dictionary)
+    @dictionary_length = dictionary.words.length
+    @dictionary = dictionary
+  end
 
+  def exact_match(term, search_area = dictionary.words)
+    midpoint = search_area.length / 2
+    possible_match = search_area[midpoint]
+    if possible_match == term
+      possible_match
+    elsif possible_match > term
+      exact_match(term, search_area[0...midpoint])
+    else
+      exact_match(term, search_area[(midpoint + 1)..-1])
+    end
   end
 
   def partial_match(term)
-
+    dictionary.select { |word| word.include?(term) }
   end
 
   def begins_match(term)
-
+    dictionary.select { |word| word.start_with?(term) }
   end
 
   def ends_match(term)
-
+    dictionary.select { |word| word.ends_with?(term) }
   end
 
 end
