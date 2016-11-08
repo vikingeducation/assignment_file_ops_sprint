9bb68efc473c91e53a16754f5dc0bad8f0ca7d58
Dexter Ford <dexter.ford.77@gmail.com>
2016-11-07 18:32:33 -0600

    Working on search options

--- lib/dictionary_searcher.rb
+++ lib/dictionary_searcher.rb
@@ -1,7 +1,7 @@
 class DictionarySearcher
   attr_reader :dictionary
 
-  def initialize(dictionary)
+  def initialize( dictionary )
     @dictionary = dictionary
   end
 
@@ -16,6 +16,35 @@ class DictionarySearcher
       frequencies[letter] = words.count
     end
     frequencies
+  end
+
+  def parse_input(search_choice, search_term)
+    case search_choice
+    when 1 then exact_search(search_term)
+    when 2 then partial_search(search_term)
+    when 3 then begins_with(search_term)
+    when 4 then ends_with(search_term)
+    end
+  end
+
+  def exact_search(search_term)
+    dictionary.words.include?(search_term)
+  end
+
+  def partial_search(search_term)
+    matches = []
+    dictionary.words.each do |word|
+      matches << word if word.include?(search_term)
+    end
+    matches
+  end
+
+  def begins_with(search_term)
+    matches = []
+    dictionary.words.each do |word|
+      matches << word if word.start_with?(search_term)
+    end
+    matches
   end
 
 end
--- lib/dictionary_ui.rb
+++ lib/dictionary_ui.rb
@@ -5,6 +5,9 @@ class DictionaryUI
     dictionary = DictionaryLoader.load(filepath)
     dictionary_searcher = DictionarySearcher.new(dictionary)
     display_dictionary_stats(dictionary_searcher)
+    choice = prompt_for_search_type
+    term = prompt_for_search_term
+    dictionary_searcher.parse_input(choice, term)
   end
 
   def gets_dictionary_filepath
@@ -24,4 +27,19 @@ class DictionaryUI
       puts "#{letter.upcase}: #{count}"
     end
   end
+
+  def prompt_for_search_type
+    puts "Whaddya wanna search for?"
+    puts "Enter 1 for exact matches search"
+    puts "Enter 2 for partial match search"
+    puts "Enter 3 for begins with search"
+    puts "Enter 4 for ends with search"
+    search_choice = gets.chomp
+  end
+
+  def prompt_for_search_term
+    puts "Enter that search criterion, yo"
+    search_term = gets.chomp
+  end
+
 end
