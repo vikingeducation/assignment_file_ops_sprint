45349dce5b886286e1cf28db276d73427c829563
Visiona <darbis@gmail.com>
2016-12-11 19:41:58 +0000

    Created DictionaryLoad and Dictionary classes

---
+++ dictionary_ui.rb
@@ -0,0 +1,78 @@
+# require 'dictionary_loader'
+# require 'dictionary'
+# require 'dictionary_searcher'
+# require 'results_saver'
+
+
+class DictionaryUI
+
+  def run
+    loop do
+      puts "Where is your dictionary? ('q' to quit)"
+      file_location = gets.chomp
+      break if file_location == 'q'
+      dictionary = Dictionary.new(file_location)
+      puts "Dictionary successfully loaded" if dictionary != nil
+      puts "Your dictionary contains #{dictionary.word_count} words."
+      puts "Word frequency by starting letter:"
+      dictionary.display_words_by_numbers
+
+
+    end
+  end
+
+end
+
+class DictionaryLoader
+  attr_accessor :file_location
+
+  def initialize(file_location)
+    @file_location = file_location
+  end
+
+  def load
+    arr = []
+    File.readlines(@file_location) do |line|
+      arr << line.strip
+    end
+  end
+
+end
+
+class Dictionary
+  attr_accessor :file
+
+  def initialize(file)
+    @file = DictionaryLoader.new(file).load
+  end
+
+  def word_count
+    @file.size
+  end
+
+  def words_count_by_first_letter
+    words_frequency = {}
+    alphabet = "ABCDEFHIJKLMNOPQRSTWXYZ".split("")
+    alphabet.each do |letter|
+      counter = 0
+      @file.each {|word| counter += 1 if word[0] == letter }
+      words_frequency[letter] = counter
+    end
+    words_frequency
+  end
+
+  def display_words_by_numbers
+    words_count_by_first_letter.each{|key, value| puts "#{key}: #{value}"}
+  end
+
+end
+
+class DictionarySearcher
+
+  
+
+end
+
+class ResultsSaver
+
+end
