89c3c6f181b70b8c67dfaeaf3d66d8c68d5b9d7b
Ann Allan <ann.cristina.allan@gmail.com>
2017-03-16 16:28:00 -0400

    Add saving functionaility

--- dictionaryUI.rb
+++ dictionaryUI.rb
@@ -2,6 +2,7 @@ class DictionaryUI
 	require './dictionary_loader'
 	require './dictionary'
 	require './dictionary_searcher'
+	require './results_saver'
 	def initialize
 	end
 
@@ -14,7 +15,15 @@ class DictionaryUI
 		ds = DictionarySearcher.new(dictionary)
 		search_type = ds.ask_search
 		search_term = ds.ask_term
-		ds.search(search_type, search_term)
+		search_results = ds.search(search_type, search_term)
+		puts "Do you want to save results? y/n? 'q' quits."
+		save = gets.chomp.downcase
+		if save == "y"
+			rs = ResultsSaver.new(search_results)
+			rs.how_save
+		else
+			puts "Thanks for using this program."
+		end
 	end
 end
 
