f5d581fbef893546b409d0c29626c0f3209c7bee
Roy Chen <roychenlei@gmail.com>
2017-03-20 21:46:22 +0800

    outlined initial specs for DictionaryUI class

---
+++ spec/dictionary_ui_spec.rb
@@ -0,0 +1,15 @@
+# spec/dictionary_ui_spec.rb
+
+describe "DictionaryUI" do
+  describe "#intialize" do
+    it "creates an instance of DictionaryUI"
+  end
+
+  describe "prompt" do
+    it "prompts the user for the location of the dictionary file"
+
+    it "exits the program if the user has entered 'q'"
+
+    it "returns the user input"
+  end
+end
