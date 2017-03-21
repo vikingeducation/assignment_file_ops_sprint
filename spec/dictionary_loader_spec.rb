c7a9a40f4b43b1b64b45b846323f669cdb9654dc
Roy Chen <roychenlei@gmail.com>
2017-03-21 14:55:24 +0800

    outlined specs for DictionaryLoader

---
+++ spec/dictionary_loader_spec.rb
@@ -0,0 +1,15 @@
+# spec/dictionary_loader_spec.rb
+
+require 'dictionary_loader'
+
+describe "DictionaryLoader" do
+  describe "initialize" do
+    it "creates an instance of DictionaryLoader"
+  end
+
+  describe "load" do
+    it "raises an error if the file does not exist"
+
+    it "returns an instance of Dictionary"
+  end
+end
