92cfeb6bfa2c4ab8dff29dd7fbbe00fc82434d9c
Roy Chen <roychenlei@gmail.com>
2017-03-21 14:15:58 +0800

    added specs/behavior for #initialize

--- spec/dictionary_ui_spec.rb
+++ spec/dictionary_ui_spec.rb
@@ -1,8 +1,14 @@
 # spec/dictionary_ui_spec.rb
 
+require 'dictionary_ui'
+
 describe "DictionaryUI" do
+  let(:dict_ui) { DictionaryUI.new }
+
   describe "#intialize" do
-    it "creates an instance of DictionaryUI"
+    it "creates an instance of DictionaryUI" do
+      expect(dict_ui).to be_a(DictionaryUI)
+    end
   end
 
   describe "prompt" do
