55de21bcd1292ead10cf44121a6c0fc6f9723396
Roy Chen <roychenlei@gmail.com>
2017-03-22 13:54:31 +0800

    implemented specs/behavior for #load to return an instance of Dictionary

--- lib/dictionary_loader.rb
+++ lib/dictionary_loader.rb
@@ -1,5 +1,8 @@
 class DictionaryLoader
   def load(filename)
     raise("That file does not exist.") unless File.exist?(filename)
+
+    words = File.readlines(filename)
+    Dictionary.new(words)
   end
 end
--- spec/dictionary_loader_spec.rb
+++ spec/dictionary_loader_spec.rb
@@ -1,6 +1,7 @@
 # spec/dictionary_loader_spec.rb
 
 require 'dictionary_loader'
+require 'dictionary'
 
 describe "DictionaryLoader" do
   let(:dict_loader) { DictionaryLoader.new }
@@ -25,6 +26,13 @@ describe "DictionaryLoader" do
       expect { dict_loader.load("blah.txt") }.to raise_error(/file does not exist/)
     end
 
-    it "returns an instance of Dictionary"
+    it "returns an instance of Dictionary" do
+      allow(File).to receive(:exist?).and_return(true)
+      allow(File).to receive(:readlines).and_return(["aaa", "bbb", "ccc"])
+
+      dict = dict_loader.load("stub.txt")
+      expect(dict).to be_a(Dictionary)
+      expect(dict.words).to eq(["aaa", "bbb", "ccc"])
+    end
   end
 end
