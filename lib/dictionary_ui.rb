bfaa0335993568e6e6585911e78de431e3a31991
Roy Chen <roychenlei@gmail.com>
2017-03-22 22:10:04 +0800

    added program loop

--- lib/dictionary_ui.rb
+++ lib/dictionary_ui.rb
@@ -1,3 +1,6 @@
+require_relative './dictionary_loader'
+require_relative './dictionary'
+
 class DictionaryUI
   def prompt
     puts "Where is your dictionary? ('q' to quit)"
@@ -5,5 +8,11 @@ class DictionaryUI
     dictionary_path = gets.chomp.downcase
 
     dictionary_path == 'q' ? exit : dictionary_path
+  end
+
+  def run
+    dictionary_path = prompt
+    dict = DictionaryLoader.new.load(dictionary_path)
+    puts dict
   end
 end
