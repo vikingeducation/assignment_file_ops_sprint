a46b3eae0a1ff564bcd640ee66121860083326a5
Roy Chen <roychenlei@gmail.com>
2017-03-23 18:32:23 +0800

    implemented spec for #initialize

--- spec/results_saver_spec.rb
+++ spec/results_saver_spec.rb
@@ -1,8 +1,14 @@
 # spec/results_saver_spec.rb
 
+require 'results_saver'
+
 describe "ResultsSaver" do
+  let(:rs) { ResultsSaver.new }
+
   describe "#initialize" do
-    it "creates an instance of ResultsSaver"
+    it "creates an instance of ResultsSaver" do
+      expect(rs).to be_a(ResultsSaver)
+    end
   end
 
   describe "#save_to_file" do
