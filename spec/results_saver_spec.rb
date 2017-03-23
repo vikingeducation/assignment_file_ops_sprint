62016f0861f9640b55491d9a9448cbf93e366d71
Roy Chen <roychenlei@gmail.com>
2017-03-23 18:29:56 +0800

    added spec outlines

---
+++ spec/results_saver_spec.rb
@@ -0,0 +1,11 @@
+# spec/results_saver_spec.rb
+
+describe "ResultsSaver" do
+  describe "#initialize" do
+    it "creates an instance of ResultsSaver"
+  end
+
+  describe "#save_to_file" do
+    it "writes the specified data to the specified file"
+  end
+end
