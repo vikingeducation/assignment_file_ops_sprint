e720c3d84e064226f97fc58f4a67d4b045eed6f7
Roy Chen <roychenlei@gmail.com>
2017-03-24 09:53:45 +0800

    modified #load to clean up special chars/whitespace in words

--- lib/dictionary_loader.rb
+++ lib/dictionary_loader.rb
@@ -3,6 +3,7 @@ class DictionaryLoader
     raise("That file does not exist.") unless File.exist?(filename)
 
     words = File.readlines(filename)
+    words.map! { |word| word.strip }
     Dictionary.new(words)
   end
 end
