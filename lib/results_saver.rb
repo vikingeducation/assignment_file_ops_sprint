d3d5d83c221196b7dc42dbc96213d8b74d71df58
Roy Chen <roychenlei@gmail.com>
2017-03-24 11:02:08 +0800

    modified file to puts instead of write

--- lib/results_saver.rb
+++ lib/results_saver.rb
@@ -1,7 +1,7 @@
 class ResultsSaver
   def save_to_file(filename, results)
     File.open(filename, 'w') do |file|
-      results.each { |result| file.write(result) }
+      results.each { |result| file.puts(result) }
     end
   end
 end
