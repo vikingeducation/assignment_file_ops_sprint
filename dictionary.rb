bd8b99c30c73e344bb6bac2a1a75896173d15cd2
Adam Jazairi <adamjazairi@gmail.com>
2017-04-02 20:03:55 -0400

    Define methods for dictionary stats

---
+++ dictionary.rb
@@ -0,0 +1,19 @@
+class Dictionary
+  attr_reader :words, :word_count
+
+  def initialize(words)
+    @words
+  end
+
+  def word_count
+    @words.length
+  end
+
+  def word_frequency
+    @occurrences = {}
+    @words.each do |word|
+      first_letter = word[0].downcase
+      @occurrences[first_letter.downcase] += 1
+    end
+  end
+end
