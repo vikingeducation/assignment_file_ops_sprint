61a48128a1dfcc0e86f9b1f0cc3dc7bfb2356533
Adam Jazairi <adamjazairi@gmail.com>
2017-04-04 15:06:08 -0400

    Complete DictionarySearcher class

--- dictionary_searcher.rb
+++ dictionary_searcher.rb
@@ -2,34 +2,44 @@ class DictionarySearcher
   attr_reader :dict
 
   def initialize(dict)
-    @dict
+    @dict = dict
   end
 
-  def search
-    # TODO: add case to determine which search method to run
+  def search(type, term)
+    case type
+    when 1
+      exact(term)
+    when 2
+      partial(term)
+    when 3
+      begins_with(term)
+    when 4
+      ends_with(term)
+    end
   end
 
   def exact(term)
-    matches = []
-    # TODO: add regex
+    @matches = []
+    @dict.each { |word| @matches << word if /^#{term}$/ =~ word }
   end
 
   def partial(term)
-    matches = []
-    # TODO: add regex
+    @matches = []
+    @dict.each { |word| @matches << word if /.*#{term}.*/ =~ word }
   end
 
   def begins_with(term)
-    matches = []
-    # TODO: add regex
+    @matches = []
+    @dict.each { |word| @matches << word if /^#{term}/ =~ word }
   end
 
   def ends_with(term)
-    matches = []
-    # TODO: add regex
+    @matches = []
+    @dict.each { |word| @matches << word if /#{term}$/ =~ word }
   end
 
   def show_results
-    # TODO: display search results
+    puts "Found #{@matches.length} matches:"
+    @matches.each { |word| puts word }
   end
 end
