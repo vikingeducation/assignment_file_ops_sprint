462b5ce05d93010a71d9cee9e7f085f44f3af14e
Miguel de Luis Espinosa <luis.miguelde@gmail.com>
2017-05-30 09:39:22 +0100

    searches work

--- d.rb
+++ d.rb
@@ -6,15 +6,33 @@ class DictionaryUI
   end
 
   def show_stats(stats)
+    puts "Dictionary successfully loaded"
     puts "Your dictionary contains #{stats[:total]} words."
     puts "Word frquency by starting letter"
     stats.each_pair {| key, value |
       puts "#{key} #{value}"
      }
   end
-    # Dictionary successfully loaded
-#
-# Word frequency by starting letter:
+
+  def search_menu
+    menu_item = nil
+    while ! ["x","p","b","e"].include?(menu_item) do
+      menu_item = input "Enter your search type: eXact, Partial, Begins with, Ends with"
+    end
+    search_term = input "Enter the term to search"
+    return menu_item, search_term
+  end
+
+  def search_response(response)
+    if response.length == 0
+      puts "Term not found"
+    else
+      puts "Term has been found #{response.length} times"
+      response.each do |word|
+        puts word
+      end
+    end
+  end
 
   private
 
@@ -40,12 +58,29 @@ end
 
 class Dictionary
   def initialize
-    interface = DictionaryUI.new
-    loader = DictionaryLoader.new
-    @contents = loader.run
-    analizer = DictionarySearcher.new
-    stats = analizer.stats(@contents)
-    interface.show_stats(stats)
+    @interface = DictionaryUI.new
+    @loader = DictionaryLoader.new
+    @contents = @loader.run
+    @analizer = DictionarySearcher.new
+    stats = @analizer.stats(@contents)
+    @interface.show_stats(stats)
+    menu_item, search_term = @interface.search_menu
+    search_hub(menu_item, search_term)
+  end
+
+  def search_hub(menu_item, search_term)
+    if menu_item == "x"
+      response = @analizer.exact_search(search_term, @contents)
+    elsif menu_item == "p"
+      response = @analizer.partial_search(search_term, @contents)
+    elsif menu_item == "b"
+      response = @analizer.begins_with(search_term, @contents)
+    elsif menu_item == "e"
+      response = @analizer.ends_with(search_term, @contents)
+    else
+      raise(ArgumentError)
+    end
+    @interface.search_response(response)
   end
 
 end
@@ -70,6 +105,36 @@ class DictionarySearcher
     stat_hash[:total] = total
     stat_hash
   end
+
+  def exact_search(term, contents)
+    response = []
+    contents.each do |word|
+      if word.downcase == term.downcase
+        response << word
+        # allows for repeated entries in dictionary, useful to find duplicates
+      end
+    end
+    response
+  end
+
+  def partial_search(term, contents)
+    string = contents.join("\n")
+    regex = Regexp.new(".*#{term}.*","i")
+    results = string.scan(regex)
+  end
+
+  def begins_with(term, contents)
+    string = contents.join("\n")
+    regex = Regexp.new("^#{term}.*","i")
+    results = string.scan(regex)
+  end
+
+  def ends_with(term, contents)
+    string = contents.join("\n")
+    regex = Regexp.new(".*#{term}$","i")
+    results = string.scan(regex)
+  end
+
 end
 
 class ResultSaver
