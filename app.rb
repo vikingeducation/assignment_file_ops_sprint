4b8e023f7634c042c8db792d430728ed7b12dcca
steven4354 <steven4354@gmail.com>
2017-09-09 15:36:58 -0700

    feat: added dictionary searching feature

--- app.rb
+++ app.rb
@@ -55,18 +55,64 @@ class DictionaryUI
 
 end
 
+#for the main page
 d = DictionaryUI.new
 d.run
 
-=begin
 class Dictionary
   #is the class which wraps the loaded dictionary.
 end
 
 class DictionarySearcher
   #is an analysis class whose sole responsibility is to perform any analysis on the dictionary you provide it.
+  def initialize()
+    p "What kind of search?"
+    p "1: Exact"
+    p "2: Partial"
+    p "3: Begins With"
+    p "4: Ends With"
+    @search = gets.chomp
+    p "Enter the search term"
+    @term = gets.chomp
+  end
+
+  def searcher()
+    if @search == "1"
+      p "Found #{$arr.select{|str| str =~ /\b#{@term}\b/}.count} matches"
+      $arr.select{|str| str =~ /\b#{@term}\b/}.each do |match|
+        p match
+      end
+      #p $arr.select{|str| str =~ /\b#{@term}\b/}
+    elsif @search == "2"
+      p "Found #{$arr.select{|str| str =~ /#{@term}/}.count} matches"
+      $arr.select{|str| str =~ /#{@term}/}.each do |match|
+        p match
+      end
+      #p $arr.select{|str| str =~ /#{@term}/}
+    elsif @search == "3"
+      p "Found #{$arr.select{|str| str =~ /^#{@term}.*$/}.count} matches"
+      $arr.select{|str| str =~ /^#{@term}.*$/}.each do |match|
+        p match
+      end
+      #p $arr.select{|str| str =~ /^#{@term}.*$/}
+    elsif @search == "4"
+      p "Found #{$arr.select{|str| str =~ /^.*#{@term}$/}.count} matches"
+      $arr.select{|str| str =~ /^.*#{@term}$/}.each do |match|
+        p match
+      end
+      #p $arr.select{|str| str =~ /^.*#{@term}$/}
+    else
+      p "search term error"
+    end
+  end
+
 end
 
+#for the main page
+start = DictionarySearcher.new
+start.searcher
+
+=begin
 class ResultsSaver
   # takes a batch of results and writes them to a file.
 end
