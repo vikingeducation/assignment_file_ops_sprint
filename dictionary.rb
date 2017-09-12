11c16cad757ac4de3c6f8a485092e103f65e685f
anne <richardson.ae@gmail.com>
2017-09-12 10:09:12 -0500

    Add loader functionality

--- dictionary.rb
+++ dictionary.rb
@@ -1,4 +1,11 @@
 # wraps the loaded dictionary
 class Dictionary
+  attr_accessor :entries
+
+  def initialize(entries)
+    @entries = entries
+  end
+
+
 
 end
--- dictionary_ui.rb
+++ dictionary_ui.rb
@@ -1,4 +1,55 @@
 # main class which handles the user interaction loop.
+require './dictionary_loader'
+require 'pry'
+
 class DictionaryUI
+  include DictionaryLoader
+
+  def run
+    request_dictionary_path
+    load_dictionary(@path)
+    puts "Press 'Enter' to see Stats."
+    gets.chomp
+    display_stats
+  end
+
+  private
+
+  @path = ''
+  @dictionary = nil
+
+  def request_dictionary_path
+    puts "Where is your dictionary? ('q' to quit)"
+    puts "ex: path/to/myfile.csv"
+    # response = gets.chomp
+    response = "./5desk.txt"
+    quit?(response)
+    puts "Great! Thanks."
+  end
+
+  def load_dictionary(path)
+    @dictionary = DictionaryLoader.create(@path)
+    puts '','Dictionary successfully loaded.'
+  end
+
+  def display_stats
+    puts "stats placeholder"
+  end
+
+  def quit?(response)
+    if response == 'q'
+      exit_program
+    else
+      @path = response
+    end
+  end
+
+  def exit_program
+    puts "kthxbai"
+    exit
+  end
 
 end
+
+d = DictionaryUI.new
+d.run
