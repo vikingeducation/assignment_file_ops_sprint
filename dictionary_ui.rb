7a0fcf78362a8a48be6a63a41811a12b469050e2
anne <richardson.ae@gmail.com>
2017-09-15 13:19:44 -0500

    Move search logic into own search class

--- dictionary_ui.rb
+++ dictionary_ui.rb
@@ -1,6 +1,7 @@
 # main class which handles the user interaction loop.
 require './dictionary_loader'
 require './dictionary_searcher'
+require './search'
 require './results_saver'
 
 class DictionaryUI
@@ -10,12 +11,8 @@ class DictionaryUI
   def run
     request_dictionary_path
     load_dictionary(@path)
-    puts "Press 'Enter' to see Stats."
-    gets.chomp
     display_stats
-    type = request_search_type
-    word = request_word
-    display_results(type, word, @dictionary)
+    search
     save_results
   end
 
@@ -41,46 +38,20 @@ class DictionaryUI
   end
 
   def display_stats
+    puts "Press 'Enter' to see Stats."
+    gets.chomp
     puts @dictionary.stats
   end
 
-  def request_search_type
-    puts '','What kind of search would you like to perform?'
-    options = SearcherFactory::SEARCH_OPTIONS
-    display_search_type_options(options)
-    response = gets.chomp
-    quit?(response)
-    until options.keys.include?(response.strip)
-      puts "I'm sorry #{response} is not an option."
-      display_search_type_options(options)
-      response = gets.chomp
-    end
-    response
-  end
-
-  def display_search_type_options(options)
-    puts 'Enter the number of one of these options:'
-    options.each {|num, option| puts "#{num}: #{option}"}
-  end
-
-  def request_word
-    puts '','Enter your word to search:'
-    response = gets.chomp.strip
-    quit?(response)
-    response
-  end
-
-  def display_results(search_type, word, dictionary)
-    puts '------------------------------'
-    puts "The results for '#{word}' are:"
-    search_results = SearcherFactory.create(search_type, word, dictionary)
-    @matches = search_results.find_matches
+  def search
+    search = Search.new(@dictionary)
+    puts search.to_s
   end
 
   def save_results
     puts '','Would you like to save these results?  y | n'
     response = gets.chomp.downcase.strip
-    quit?(response)
+    exit_program unless response == 'y'
     @filename = request_output_filename
     file_exists?(@filename) ? @permission = false : @permission = true
       while !@permission
