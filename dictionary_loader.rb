05a9f7ac08585e839b9cebfc0ba79f8e766dcef6
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2017-12-27 15:00:55 -0500

    d_ui and d_load mostly done

--- dictionary_loader.rb
+++ dictionary_loader.rb
@@ -4,5 +4,19 @@
  in the dictionary.
 =end
 
+require "./dictionary.rb"
+
 class DictionaryLoader
+
+  def initialize(path)
+    @path = path
+    fetch
+  end
+
+  def fetch
+    # need to do something about files that can't be found
+    file = File.readlines("#{@path}")
+    Dictionary(file)
+  end
+
 end
--- dictionary_ui.rb
+++ dictionary_ui.rb
@@ -1,5 +1,33 @@
 
 # DictionaryUI is the main class which handles the user interaction loop.
 
+require "./dictionary_loader.rb"
+
 class DictionaryUI
+
+  def initialize
+    locate
+  end
+
+  def locate
+    puts "Where is your dictionary?"
+    puts " Please enter the file name and location in the following format"
+    puts " path/to/myfile.csv"
+    puts " Or enter q to quit\n\n"
+    path = gets.chomp
+    quit?(path)
+    load(path)
+  end
+
+  def quit?(input)
+    exit if input == "q"
+  end
+
+  def load(file_path)
+    @dictionary = DictionaryLoader.new(file_path)
+    puts @dictionary
+  end
+
 end
+
+d = DictionaryUI.new
