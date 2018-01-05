d699f769745cc3cb06740417da4605f5668cf47a
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-01-05 06:34:11 -0500

    reworked how results_saver checks itself, since it will only ever be looking at a single variable

--- results_saver.rb
+++ results_saver.rb
@@ -14,19 +14,19 @@
      Dir.chdir(File.dirname(@where)) do
        File.open(File.basename(@where), mode) { |file| file.write(@results) }
      end
-     target_exist(@where)
+     target_exist
    end
 
-   def target_exist(file_path)
-     if File.file?(file_path)
-       write_check(file_path)
+   def target_exist
+     if File.file?(@where)
+       write_check
      else
        puts "\n Unable to create file to save results to"
      end
    end
 
-   def write_check(file_path)
-     if File.open(file_path).each.any? { |line| line.include?("#{@results}") }
+   def write_check
+     if File.open(@where).each.any? { |line| line.include?("#{@results}") }
        puts "\n Results saved successfully"
      else
        puts "\n Failed to save results"
